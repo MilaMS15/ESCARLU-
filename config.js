3e5063f81d9f2a16d56400aac6741b1c081587a0
MilaMS15 <milams.15102005@gmail.com>
2026-07-23 22:29:28 +0000

    Deploying to gh-pages from @ MilaMS15/ESCARLU-@6ecab4c770c0f2c84a695b6dfb0a727dceffbf3d 🚀

---
+++ config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://pbqplcasneqkhvvywqnf.supabase.co";
+const SUPABASE_KEY = "sb_publishable_mci9sv9nfjjB_CsjfFoALg_8Miy9_b3";
+if (window.supabase) { window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY); }
